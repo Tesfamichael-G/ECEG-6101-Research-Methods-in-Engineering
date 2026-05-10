ca8fed165dcfd9b6849b8fc531759c48f5a8218e
Tesfamichael-G <tesfamichael.gegziabher@aait.edu.et>
2026-05-10 19:47:12 +0300

    V2

--- lecture-notes/sidebar.js
+++ lecture-notes/sidebar.js
@@ -1,20 +1,37 @@
-// sidebar.js
+// sidebar.js                      
+// // <li><a href="semantic-comm-papers.html">Semantic Comm. Papers</a></li>
+//         <h3>ECEG-6101 Modules</h3>
 document.addEventListener("DOMContentLoaded", function () {
     const sidebarHTML = `
-        <h3>ECEG-6101 Modules</h3>
         <ul>
             <li><a href="../index.html">🏠 Course Home</a></li>
             <li><a href="1. introduction.html">1. Concept of Research</a></li>
             <li><a href="2. research-types.html">2. Types of Research</a></li>
-            <li><a href="3. process.html">3. Research Process</a></li>
-            <li><a href="4. proposal.html">4. Proposal Development</a></li>
+            <li>
+                <a href="3. process.html">3. Research Process</a>
+                <ul  style="margin-left: 20px;">
+                    <li><a href="open-access-papers.html">Open Access Papers</a></li>
+                    <li><a href="ieee-papers-list.html">IEEE Papers List</a></li>
+                </ul
+            </li>
+            <li>
+                <a href="4. proposal.html">4. Proposal Development</a>
+                <ul  style="margin-left: 20px;">
+                    <li><a href="example-concept-note.html">Example: Concept Note</a></li>
+                </ul
+            </li>
+
             <li><a href="5. ethics.html">5. Research Ethics</a></li>
-            <li><a href="6. reporting.html">6. Technical Reporting</a></li>
+            <li>
+            <a href="6. reporting.html">6. Technical Reporting</a>
+                <ul style="margin-left: 20px;">
+                    <li><a href="example-technical-report.html">Example: Technical Report</a></li>
+                </ul
+            </li>
         </ul>
-        <hr style="border-color: rgba(255,255,255,0.2);">
         <a href="practice.html">Practice Questions</a>
     `;
-
+//      <hr style="border-color: rgba(255,255,255,0.2);">
     // Insert Sidebar
     const sidebarContainer = document.getElementById('sidebar');
     if (sidebarContainer) {
